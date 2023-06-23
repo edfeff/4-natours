ba9bd59c79a968d5eb2f6975e33cbbe020e27971
edfeff <edfeff@163.com>
2023-06-24 07:40:34 +0800

    default router

--- app.js
+++ app.js
@@ -24,4 +24,14 @@ app.use(express.static(`${__dirname}/public`))
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// 默认异常处理
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `未知路径: ${req.originalUrl}`
+  })
+})
+
+
+
 module.exports = app;
